10f7407a0dfdd3b29f27c9cb6c496a763dc2208b
h-sumeet <124994281+h-sumeet@users.noreply.github.com>
2023-03-03 14:53:40 +0530

    Add files via upload

---
+++ Json_file/src/Address.java
@@ -0,0 +1,10 @@
+
+public class Address {
+    String state;
+    String pincode;
+
+    public Address(String state, String pincode) {
+        this.state = state;
+        this.pincode = pincode;
+    }
+}
